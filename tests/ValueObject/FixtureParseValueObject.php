4642ba9f992f83ae6378380aab5a83590e369eca
roosiu <piotr.rosiak@totem.com.pl>
2024-09-04 10:37:44 +0200

    tests: Refactor `ParseValueObjectTest`

---
+++ tests/ValueObject/FixtureParseValueObject.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Totem\SamSkeleton\Tests\ValueObject;
+
+use Totem\SamSkeleton\Bundles\ValueObject\ParseValueObject;
+
+class FixtureParseValueObject
+{
+    use ParseValueObject;
+}
