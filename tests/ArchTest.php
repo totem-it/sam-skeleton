f23f61a63e9b1c934de8a372506cd8cd6fef8995
roosiu <piotr.rosiak@totem.com.pl>
2024-09-24 14:50:12 +0200

    tests(#SAM-344): Added `ArchTest.php`

---
+++ tests/ArchTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Totem\SamSkeleton\Tests;
+
+arch()->preset()->php();
+
+arch()->preset()->security();
+
+arch('no debug')
+    ->expect('Totem\SamSkeleton')
+    ->not->toUse(['die', 'dd', 'dump', 'var_dump']);
+
+arch('strict types')
+    ->expect('Totem\SamSkeleton')
+    ->toUseStrictTypes();
+
+arch('strict equality')
+    ->expect('Totem\SamSkeleton')
+    ->toUseStrictEquality();
