73e1a6fb9209478066328a93c953d84b8414cd24
roosiu <piotr.rosiak@totem.com.pl>
2024-10-01 10:04:30 +0200

    refactor(#SAM-337): Replaced `ForceJsonMiddleware` from `SamCore` to `SamSkeleton`

--- src/SamSkeletonServiceProvider.php
+++ src/SamSkeletonServiceProvider.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace Totem\SamSkeleton;
 
 use Illuminate\Support\ServiceProvider;
+use Totem\SamSkeleton\Bundles\Middleware\ForceJsonMiddleware;
 
 class SamSkeletonServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        $this->app['router']->prependMiddlewareToGroup('api', ForceJsonMiddleware::class);
+    }
 }
