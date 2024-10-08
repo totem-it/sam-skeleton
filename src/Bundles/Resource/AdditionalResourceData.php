0476714fceec3ede2ee078ae7d2c5801495a8392
roosiu <piotr.rosiak@totem.com.pl>
2024-10-08 09:29:41 +0200

    chore: Added phpdoc in `AdditionalResourceData`

--- src/Bundles/Resource/AdditionalResourceData.php
+++ src/Bundles/Resource/AdditionalResourceData.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Totem\SamSkeleton\Bundles\Resource;
 
+/**
+ * @property \Totem\SamSkeleton\Bundles\Resource\ApiResource $resource
+ * @property \Totem\SamSkeleton\Bundles\Resource\ApiCollection $collection
+ */
 trait AdditionalResourceData
 {
     public function with($request): array
