176367ddfbec3bd7837dd970c92fe9e467781718
roosiu <piotr.rosiak@totem.com.pl>
2024-12-02 09:54:59 +0100

    test: Added new tests in `ArchTest`

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Totem\SamSkeleton\Tests;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Totem\SamSkeleton\Bundles\Resource\ApiCollection;
+use Totem\SamSkeleton\Bundles\Resource\ApiResource;
+use Totem\SamSkeleton\SamSkeletonServiceProvider;
+
 //arch()->preset()->php();
 
 //arch()->preset()->security();
@@ -12,6 +18,11 @@ arch('no debug')
     ->expect('Totem\SamSkeleton')
     ->not->toUse(['die', 'dd', 'dump', 'var_dump']);
 
+arch('no env()')
+    ->expect('Totem\SamSkeleton')
+    ->not()->toUse('env')
+    ->ignoring(SamSkeletonServiceProvider::class);
+
 arch('strict types')
     ->expect('Totem\SamSkeleton')
     ->toUseStrictTypes();
@@ -19,3 +30,18 @@ arch('strict types')
 arch('strict equality')
     ->expect('Totem\SamSkeleton')
     ->toUseStrictEquality();
+
+arch('Bundle Middleware')
+    ->expect('Totem\SamSkeleton\Bundles\Middleware')
+    ->toHaveMethod('handle')
+    ->toHaveSuffix('Middleware');
+
+describe('Bundle Resource', function (): void {
+    arch('resource')
+        ->expect(ApiResource::class)
+        ->toExtend(JsonResource::class);
+
+    arch('collection')
+        ->expect(ApiCollection::class)
+        ->toExtend(ResourceCollection::class);
+});
